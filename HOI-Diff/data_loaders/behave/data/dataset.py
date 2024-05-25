fixed bugs with normalization and loss vector lengths

HOI-Diff/data_loaders/behave/data/dataset.py
@@ -181,8 +181,11 @@ class Text2AffordDataset(data.Dataset):
 
 class TextOnlyDataset(data.Dataset):
     def __init__(self, opt, mean, std, split_file):
-        self.mean = mean[: opt.dim_pose]
-        self.std = std[: opt.dim_pose]
+        # more length limiting for some reason - disable this
+        # self.mean = mean[: opt.dim_pose]
+        # self.std = std[: opt.dim_pose]
+        self.mean = mean
+        self.std = std
         self.opt = opt
         self.data_dict = []
         self.max_length = 20
@@ -303,7 +306,9 @@ class TextOnlyDataset(data.Dataset):
 
     def inv_transform(self, data):
         data = data.clone()
-        if data.shape[-1] == 269:
+        if data.shape[-1] == 269 + 2 * HAND_FEATURE_DIM:
+            assert len(self.mean) == 269 + 2 * HAND_FEATURE_DIM, f"Mean feature dim {len(self.mean)} is not matching"
+            assert len(self.std) == 269 + 2 * HAND_FEATURE_DIM, f"Std feature dim {len(self.std)} is not matching"
             data = data * self.std + self.mean
         else:
             data[..., :263] = data[..., :263] * self.std[:263] + self.mean[:263]
@@ -362,14 +367,13 @@ class Text2MotionDatasetV2(data.Dataset):
         data_dict = {}
         id_list = []
 
-
         with cs.open(split_file, "r") as f:
             for line in f.readlines():
                 id_list.append(line.strip())
 
         new_name_list = []
         length_list = []
-        print(f'Length of id_list: {len(id_list)}')
+        print(f"Length of id_list: {len(id_list)}")
         for name in tqdm(id_list):
             try:
                 motion = np.load(pjoin(opt.motion_dir, name + ".npy"))
@@ -565,15 +569,15 @@ class Text2MotionDatasetV2(data.Dataset):
         if not self.opt.use_global:
             "Z Normalization"
             motion = np.copy(motion)
-            if len(self.mean) == 269 + 2*HAND_FEATURE_DIM:
+            if len(self.mean) == 269 + 2 * HAND_FEATURE_DIM:
                 np.seterr(all="raise")
                 # print(f'Length of batch: {motion.shape[0]}')
                 # print(f'Std. deviation: {np.mean(self.std)}')
                 try:
-                    end_idx = 269 + 2*HAND_FEATURE_DIM
-                    motion[:, :end_idx] = (motion[:, :end_idx] - self.mean[:end_idx]) / self.std[
-                        :end_idx
-                    ]
+                    end_idx = 269 + 2 * HAND_FEATURE_DIM
+                    motion[:, :end_idx] = (
+                        motion[:, :end_idx] - self.mean[:end_idx]
+                    ) / self.std[:end_idx]
                 except FloatingPointError as err:
                     print(err.__class__.__name__)
                     print(err)
@@ -663,18 +667,22 @@ class Behave(data.Dataset):
 
         elif self.training_stage == 2:
             if mode == "gt":
+                print('Loading ground truth mean and std')
                 # used by T2M models (including evaluators)
                 self.mean = np.load(pjoin(opt.meta_dir, f"t2m_mean.npy"))
                 self.std = np.load(pjoin(opt.meta_dir, f"t2m_std.npy"))
 
             elif mode in ["train", "eval", "text_only"]:
                 # used by our models
+                print(f'Loading local mean and std from {opt.data_root}')
                 self.mean = np.load(pjoin(opt.data_root, "Mean_local.npy"))
                 self.std = np.load(pjoin(opt.data_root, "Std_local.npy"))
+                print(f"Mean: {self.mean.shape}  Std: {self.std.shape}")
 
             if mode == "eval":
                 # used by T2M models (including evaluators)
                 # this is to translate their norms to ours
+                print('Loading eval mean and std')
 
                 self.mean_for_eval = np.load(pjoin(opt.meta_dir, f"t2m_mean.npy"))
                 self.std_for_eval = np.load(pjoin(opt.meta_dir, f"t2m_std.npy"))

HOI-Diff/model/hoi_diff.py
@@ -8,7 +8,12 @@ import clip
 from model.mdm import MDM
 from model.mdm import *
 
-NUM_HAND_FEATURES = 63
+HAND_MODE = "joints"
+if HAND_MODE == "PCA":
+    HAND_FEATURE_DIM = 30
+elif HAND_MODE == "joints":
+    HAND_FEATURE_DIM = 63
+
 
 class HOIDiff(MDM):
     def __init__(
@@ -40,7 +45,7 @@ class HOIDiff(MDM):
     ):
         super(HOIDiff, self).__init__(
             modeltype,
-            njoints - (6 + 2 * NUM_HAND_FEATURES),
+            njoints - (6 + 2 * HAND_FEATURE_DIM),
             nfeats,
             num_actions,
             translation,
@@ -104,7 +109,6 @@ class HOIDiff(MDM):
             activation=self.activation,
         )
 
-
         self.seqTransEncoder_obj_pose = nn.TransformerEncoder(
             seqTransEncoderLayer_obj_pose, num_layers=2
         )
@@ -126,24 +130,23 @@ class HOIDiff(MDM):
         )
 
         # we add 2*(24 + 6) for both 24-dim PCA hand params and 6-dim global rotation
-        self.input_process_obj = InputProcess(
-            self.data_rep, 6, self.latent_dim
-        )
+        self.input_process_obj = InputProcess(self.data_rep, 6, self.latent_dim)
 
         self.output_process_obj = OutputProcess(
             self.data_rep, 6, self.latent_dim, 6, self.nfeats
         )
 
         self.input_process_hand = InputProcess(
-            self.data_rep, 2 * NUM_HAND_FEATURES, self.latent_dim
+            self.data_rep, 2 * HAND_FEATURE_DIM, self.latent_dim
         )
 
         self.output_process_hand = OutputProcess(
-            self.data_rep, 2 * NUM_HAND_FEATURES, self.latent_dim, 2 * NUM_HAND_FEATURES, self.nfeats
+            self.data_rep,
+            2 * HAND_FEATURE_DIM,
+            self.latent_dim,
+            2 * HAND_FEATURE_DIM,
+            self.nfeats,
         )
-
-
-
 
     def mask_cond_obj(self, cond, force_mask=False):
         seq, bs, d = cond.shape
@@ -200,7 +203,6 @@ class HOIDiff(MDM):
         xseq_hands = self.sequence_pos_encoder(xseq_hands)
         hands_mid = self.seqTransEncoder_hands(xseq_hands)
 
-
         if self.args.multi_backbone_split < self.num_layers:
             dec_output_human, dec_output_obj = self.mutual_attn(
                 human_mid[1:], obj_mid[1:]
@@ -209,9 +211,7 @@ class HOIDiff(MDM):
             # another pairwise attention
             # TODO: change the code such that hands attend to body, but not the other way around
             # or extend the attention here to be pairwise
-            _, dec_output_hands = self.mutual_attn(
-                human_mid[1:], hands_mid[1:]
-            )
+            _, dec_output_hands = self.mutual_attn(human_mid[1:], hands_mid[1:])
 
             output_human = self.seqTransEncoder_end(
                 torch.cat([human_mid[:1], dec_output_human], 0)

HOI-Diff/train/training_loop.py
@@ -16,6 +16,7 @@ from diffusion.resample import LossAwareSampler, UniformSampler
 from tqdm import tqdm
 from diffusion.resample import create_named_schedule_sampler
 from data_loaders.behave.networks.evaluator_wrapper import EvaluatorMDMWrapper
+
 # from eval import eval_behave
 from data_loaders.get_data import get_dataset_loader
 
@@ -48,7 +49,7 @@ class TrainLoop:
 
         self.step = 0
         self.resume_step = 0
-        self.global_batch = self.batch_size # * dist.get_world_size()
+        self.global_batch = self.batch_size  # * dist.get_world_size()
         self.num_steps = args.num_steps
         self.num_epochs = self.num_steps // len(self.data) + 1
 
@@ -73,28 +74,44 @@ class TrainLoop:
             # being specified at the command line.
 
         self.device = torch.device("cpu")
-        if torch.cuda.is_available() and dist_util.dev() != 'cpu':
+        if torch.cuda.is_available() and dist_util.dev() != "cpu":
             self.device = torch.device(dist_util.dev())
 
-        self.schedule_sampler_type = 'uniform'
-        self.schedule_sampler = create_named_schedule_sampler(self.schedule_sampler_type, diffusion)
+        self.schedule_sampler_type = "uniform"
+        self.schedule_sampler = create_named_schedule_sampler(
+            self.schedule_sampler_type, diffusion
+        )
         self.eval_wrapper, self.eval_data, self.eval_gt_data = None, None, None
-        if args.dataset in ['kit', 'humanml'] and args.eval_during_training:
+        if args.dataset in ["kit", "humanml"] and args.eval_during_training:
             mm_num_samples = 0  # mm is super slow hence we won't run it during training
             mm_num_repeats = 0  # mm is super slow hence we won't run it during training
-            gen_loader = get_dataset_loader(name=args.dataset, batch_size=args.eval_batch_size, num_frames=None,
-                                            split=args.eval_split,
-                                            hml_mode='eval')
+            gen_loader = get_dataset_loader(
+                name=args.dataset,
+                batch_size=args.eval_batch_size,
+                num_frames=None,
+                split=args.eval_split,
+                hml_mode="eval",
+            )
 
-            self.eval_gt_data = get_dataset_loader(name=args.dataset, batch_size=args.eval_batch_size, num_frames=None,
-                                                   split=args.eval_split,
-                                                   hml_mode='gt')
+            self.eval_gt_data = get_dataset_loader(
+                name=args.dataset,
+                batch_size=args.eval_batch_size,
+                num_frames=None,
+                split=args.eval_split,
+                hml_mode="gt",
+            )
             self.eval_wrapper = EvaluatorMDMWrapper(args.dataset, dist_util.dev())
             self.eval_data = {
-                'test': lambda: eval_behave.get_mdm_loader(
-                    model, diffusion, args.eval_batch_size,
-                    gen_loader, mm_num_samples, mm_num_repeats, gen_loader.dataset.opt.max_motion_length,
-                    args.eval_num_samples, scale=1.,
+                "test": lambda: eval_behave.get_mdm_loader(
+                    model,
+                    diffusion,
+                    args.eval_batch_size,
+                    gen_loader,
+                    mm_num_samples,
+                    mm_num_repeats,
+                    gen_loader.dataset.opt.max_motion_length,
+                    args.eval_num_samples,
+                    scale=1.0,
                 )
             }
         self.use_ddp = False
@@ -125,26 +142,37 @@ class TrainLoop:
             self.opt.load_state_dict(state_dict)
 
     def run_loop(self):
-
         for epoch in range(self.num_epochs):
-            print(f'Starting epoch {epoch}')
+            print(f"Starting epoch {epoch}")
             for motion, cond in tqdm(self.data):
-                if not (not self.lr_anneal_steps or self.step + self.resume_step < self.lr_anneal_steps):
+                if not (
+                    not self.lr_anneal_steps
+                    or self.step + self.resume_step < self.lr_anneal_steps
+                ):
                     break
 
                 motion = motion.to(self.device)
-                cond['y'] = {key: val.to(self.device) if torch.is_tensor(val) else val for key, val in cond['y'].items()}
+                cond["y"] = {
+                    key: val.to(self.device) if torch.is_tensor(val) else val
+                    for key, val in cond["y"].items()
+                }
 
                 self.run_step(motion, cond)
                 if self.step % self.log_interval == 0:
-                    for k,v in logger.get_current().name2val.items():
-                        if k == 'loss':
-                            print('step[{}]: loss[{:0.5f}]'.format(self.step+self.resume_step, v))
+                    for k, v in logger.get_current().name2val.items():
+                        if k == "loss":
+                            print(
+                                "step[{}]: loss[{:0.5f}]".format(
+                                    self.step + self.resume_step, v
+                                )
+                            )
 
-                        if k in ['step', 'samples'] or '_q' in k:
+                        if k in ["step", "samples"] or "_q" in k:
                             continue
                         else:
-                            self.train_platform.report_scalar(name=k, value=v, iteration=self.step, group_name='Loss')
+                            self.train_platform.report_scalar(
+                                name=k, value=v, iteration=self.step, group_name="Loss"
+                            )
 
                 if self.step % self.save_interval == 0:
                     self.save()
@@ -156,7 +184,10 @@ class TrainLoop:
                     if os.environ.get("DIFFUSION_TRAINING_TEST", "") and self.step > 0:
                         return
                 self.step += 1
-            if not (not self.lr_anneal_steps or self.step + self.resume_step < self.lr_anneal_steps):
+            if not (
+                not self.lr_anneal_steps
+                or self.step + self.resume_step < self.lr_anneal_steps
+            ):
                 break
         # Save the last checkpoint if it wasn't already saved.
         if (self.step - 1) % self.save_interval != 0:
@@ -168,23 +199,39 @@ class TrainLoop:
             return
         start_eval = time.time()
         if self.eval_wrapper is not None:
-            print('Running evaluation loop: [Should take about 90 min]')
-            log_file = os.path.join(self.save_dir, f'eval_behave_{(self.step + self.resume_step):09d}.log')
+            print("Running evaluation loop: [Should take about 90 min]")
+            log_file = os.path.join(
+                self.save_dir, f"eval_behave_{(self.step + self.resume_step):09d}.log"
+            )
             diversity_times = 300
             mm_num_times = 0  # mm is super slow hence we won't run it during training
             eval_dict = eval_behave.evaluation(
-                self.eval_wrapper, self.eval_gt_data, self.eval_data, log_file,
-                replication_times=self.args.eval_rep_times, diversity_times=diversity_times, mm_num_times=mm_num_times, run_mm=False)
+                self.eval_wrapper,
+                self.eval_gt_data,
+                self.eval_data,
+                log_file,
+                replication_times=self.args.eval_rep_times,
+                diversity_times=diversity_times,
+                mm_num_times=mm_num_times,
+                run_mm=False,
+            )
             print(eval_dict)
             for k, v in eval_dict.items():
-                if k.startswith('R_precision'):
+                if k.startswith("R_precision"):
                     for i in range(len(v)):
-                        self.train_platform.report_scalar(name=f'top{i + 1}_' + k, value=v[i],
-                                                          iteration=self.step + self.resume_step,
-                                                          group_name='Eval')
+                        self.train_platform.report_scalar(
+                            name=f"top{i + 1}_" + k,
+                            value=v[i],
+                            iteration=self.step + self.resume_step,
+                            group_name="Eval",
+                        )
                 else:
-                    self.train_platform.report_scalar(name=k, value=v, iteration=self.step + self.resume_step,
-                                                      group_name='Eval')
+                    self.train_platform.report_scalar(
+                        name=k,
+                        value=v,
+                        iteration=self.step + self.resume_step,
+                        group_name="Eval",
+                    )
 
         # elif self.dataset in ['humanact12', 'uestc']:
         #     eval_args = SimpleNamespace(num_seeds=self.args.eval_rep_times, num_samples=self.args.eval_num_samples,
@@ -200,8 +247,7 @@ class TrainLoop:
         #             self.train_platform.report_scalar(name=k, value=np.array(v).astype(float).mean(), iteration=self.step, group_name='Eval Unconstrained')
 
         end_eval = time.time()
-        print(f'Evaluation time: {round(end_eval-start_eval)/60}min')
-
+        print(f"Evaluation time: {round(end_eval-start_eval)/60}min")
 
     def run_step(self, batch, cond):
         self.forward_backward(batch, cond)
@@ -210,8 +256,7 @@ class TrainLoop:
         self.log_step()
 
     def compute_losses(self, micro, t, micro_cond):
-
-        seq_mask = micro_cond['y']['mask']
+        seq_mask = micro_cond["y"]["mask"]
         output = self.diffusion.training_losses(
             model=self.ddp_model,
             x_start=micro,  # [bs, ch, image_size, image_size]
@@ -238,12 +283,11 @@ class TrainLoop:
             last_batch = (i + self.microbatch) >= batch.shape[0]
             t, weights = self.schedule_sampler.sample(micro.shape[0], dist_util.dev())
 
-
             if last_batch or not self.use_ddp:
-                losses = self.compute_losses(micro,t, micro_cond)
+                losses = self.compute_losses(micro, t, micro_cond)
             else:
                 with self.ddp_model.no_sync():
-                    losses = self.compute_losses(micro,t, micro_cond)
+                    losses = self.compute_losses(micro, t, micro_cond)
 
             if isinstance(self.schedule_sampler, LossAwareSampler):
                 self.schedule_sampler.update_with_local_losses(
@@ -268,17 +312,15 @@ class TrainLoop:
         logger.logkv("step", self.step + self.resume_step)
         logger.logkv("samples", (self.step + self.resume_step + 1) * self.global_batch)
 
-
     def ckpt_file_name(self):
         return f"model{(self.step+self.resume_step):09d}.pt"
-
 
     def save(self):
         def save_checkpoint(params):
             state_dict = self.mp_trainer.master_params_to_state_dict(params)
 
             # Do not save CLIP weights
-            clip_weights = [e for e in state_dict.keys() if e.startswith('clip_model.')]
+            clip_weights = [e for e in state_dict.keys() if e.startswith("clip_model.")]
             for e in clip_weights:
                 del state_dict[e]
 
@@ -326,6 +368,7 @@ def find_resume_checkpoint():
 def log_loss_dict(diffusion, ts, losses):
     for key, values in losses.items():
         logger.logkv_mean(key, values.mean().item())
+        # logger.log(f"loss key: {key}, mean: {values.mean().item()}, shape: {values.shape}")
         # Log the quantiles (four quartiles, in particular).
         for sub_t, sub_loss in zip(ts.cpu().numpy(), values.detach().cpu().numpy()):
             quartile = int(4 * sub_t / diffusion.num_timesteps)